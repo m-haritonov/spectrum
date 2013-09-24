c0ba4e0266bb6c40242e287560f40c10704ce0ef
Mikhail Kharitonov <mail@mkharitonov.net>
2013-09-25 01:46:03 +0400

    Create tests for "MatcherCallDetails" class

--- spectrum/core/MatcherCallDetails.php
+++ spectrum/core/MatcherCallDetails.php
@@ -22,37 +22,23 @@ class MatcherCallDetails implements MatcherCallDetailsInterface
 	public function setTestedValue($testedValue){ $this->testedValue = $testedValue; }
 	public function getTestedValue(){ return $this->testedValue; }
 
-/**/
-
 //	public function setTestedValueSourceCode($sourceCode){ $this->testedValueSourceCode = $sourceCode; }
 //	public function getTestedValueSourceCode(){ return $this->testedValueSourceCode; }
-
-/**/
 
 	public function setNot($not){ $this->not = $not; }
 	public function getNot(){ return $this->not; }
 
-/**/
-
 	public function setResult($result){ $this->result = $result; }
 	public function getResult(){ return $this->result; }
-
-/**/
 
 	public function setMatcherName($matcherName){ $this->matcherName = $matcherName; }
 	public function getMatcherName(){ return $this->matcherName; }
 
-/**/
-
 	public function setMatcherArguments(array $matcherArguments){ $this->matcherArguments = $matcherArguments; }
 	public function getMatcherArguments(){ return $this->matcherArguments; }
 
-/**/
-
 	public function setMatcherReturnValue($matcherReturnValue){ $this->matcherReturnValue = $matcherReturnValue; }
 	public function getMatcherReturnValue(){ return $this->matcherReturnValue; }
-	
-/**/
 	
 	public function setMatcherException(\Exception $exception = null){ $this->matcherException = $exception; }
 	public function getMatcherException(){ return $this->matcherException; }
---
+++ tests/core/MatcherCallDetailsTest.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * (c) Mikhail Kharitonov <mail@mkharitonov.net>
+ *
+ * For the full copyright and license information, see the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace spectrum\tests\core;
+
+use spectrum\core\MatcherCallDetails;
+
+require_once __DIR__ . '/../init.php';
+
+class MatcherCallDetailsTest extends \spectrum\tests\Test
+{
+	public function testReturnsSetValues()
+	{
+		$exception = new \Exception();
+		
+		$matcherCallDetails = new MatcherCallDetails();
+		$matcherCallDetails->setTestedValue('aaa');
+		$matcherCallDetails->setNot(true);
+		$matcherCallDetails->setResult('bbb');
+		$matcherCallDetails->setMatcherName('ccc');
+		$matcherCallDetails->setMatcherArguments(array('ddd', 'eee', 'fff'));
+		$matcherCallDetails->setMatcherReturnValue('ggg');
+		$matcherCallDetails->setMatcherException($exception);
+		
+		$this->assertSame('aaa', $matcherCallDetails->getTestedValue());
+		$this->assertSame(true, $matcherCallDetails->getNot());
+		$this->assertSame('bbb', $matcherCallDetails->getResult());
+		$this->assertSame('ccc', $matcherCallDetails->getMatcherName());
+		$this->assertSame(array('ddd', 'eee', 'fff'), $matcherCallDetails->getMatcherArguments());
+		$this->assertSame('ggg', $matcherCallDetails->getMatcherReturnValue());
+		$this->assertSame($exception, $matcherCallDetails->getMatcherException());
+		
+		$matcherCallDetails->setNot(false);
+		$matcherCallDetails->setMatcherException(null);
+		
+		$this->assertSame(false, $matcherCallDetails->getNot());
+		$this->assertSame(null, $matcherCallDetails->getMatcherException());
+	}
+	
+	public function testReturnsEmptyValuesByDefault()
+	{
+		$matcherCallDetails = new MatcherCallDetails();
+		
+		$this->assertSame(null, $matcherCallDetails->getTestedValue());
+		$this->assertSame(null, $matcherCallDetails->getNot());
+		$this->assertSame(null, $matcherCallDetails->getResult());
+		$this->assertSame(null, $matcherCallDetails->getMatcherName());
+		$this->assertSame(array(), $matcherCallDetails->getMatcherArguments());
+		$this->assertSame(null, $matcherCallDetails->getMatcherReturnValue());
+		$this->assertSame(null, $matcherCallDetails->getMatcherException());
+	}
+}
