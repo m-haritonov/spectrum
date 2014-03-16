9b4e2ddb53e16ecb69eecd0e24e39b33508f7497
Mikhail Kharitonov <mail@mkharitonov.net>
2014-03-16 15:46:22 +0400

    Add function value support to array context

--- spectrum/_internal/convertArrayWithContextsToSpecs.php
+++ spectrum/_internal/convertArrayWithContextsToSpecs.php
@@ -14,33 +14,38 @@ function convertArrayWithContextsToSpecs(array $contexts)
 {
 	$specClass = config::getClassReplacement('\spectrum\core\Spec');
 	$specs = array();
+	$closure = function(){};
 	
 	$num = 0;
 	foreach ($contexts as $title => $values)
 	{
 		$num++;
-		
-		if (!is_array($values))
-			throw new Exception('The context row #' . $num . ' should be an array');
-
-		$firstValue = reset($values);
-		if ((!is_string($title) || $title === '') && count($values) >= 1 && is_scalar($firstValue))
+		if (is_array($values))
 		{
-			if (mb_strlen($firstValue, config::getInputCharset()) > 100)
-				$title = mb_substr($firstValue, 0, 100, config::getInputCharset()) . '...';
-			else
-				$title = $firstValue;
+			$firstValue = reset($values);
+			if ((!is_string($title) || $title === '') && count($values) >= 1 && is_scalar($firstValue))
+			{
+				if (mb_strlen($firstValue, config::getInputCharset()) > 100)
+					$title = mb_substr($firstValue, 0, 100, config::getInputCharset()) . '...';
+				else
+					$title = $firstValue;
+			}
+			
+			$contextModifierFunction = function() use($values){
+				$getContextDataFunction = config::getFunctionReplacement('\spectrum\_internal\getContextData');
+				$contextData = $getContextDataFunction();
+				foreach ($values as $propertyName => $value)
+					$contextData->$propertyName = $value;
+			};
 		}
+		else if ($values instanceof $closure)
+			$contextModifierFunction = $values;
+		else
+			throw new Exception('The context row #' . $num . ' should be an array');
 		
 		$spec = new $specClass();
 		$spec->setName($title);
-		$spec->contextModifiers->add(function() use($values){
-			$getContextDataFunction = config::getFunctionReplacement('\spectrum\_internal\getContextData');
-			$contextData = $getContextDataFunction();
-			foreach ($values as $propertyName => $value)
-				$contextData->$propertyName = $value;
-		}, 'before');
-		
+		$spec->contextModifiers->add($contextModifierFunction, 'before');
 		$specs[] = $spec;
 	}
 	
--- tests/_internal/ConvertArrayWithContextsToSpecsTest.php
+++ tests/_internal/ConvertArrayWithContextsToSpecsTest.php
@@ -11,12 +11,135 @@ require_once __DIR__ . '/../init.php';
 
 class ConvertArrayWithContextsToSpecsTest extends \spectrum\tests\Test
 {
-	public function testCallsAtBuildingState_ReturnsSpecTreeWithBeforeContextFunctions()
+	public function testCallsAtBuildingState_ElementIsArray_ReturnsSpecsWithBeforeContextFunctionsThatSetsValuesToContextData()
 	{
 		$specs = \spectrum\_internal\convertArrayWithContextsToSpecs(array(
 			array(),
 			array('aaa'),
 			array('bbb', 'ccc'),
+			array('aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'),
+			array('bbb1', 'bbb2' => 'bbb3', 'bbb4' => 'bbb5'),
+			array('ccc1', 'ccc2' => 'ccc3', 'ccc4' => 'ccc5'),
+		));
+		
+		$this->assertSame(6, count($specs));
+		
+		$contexts = $specs[0]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[1]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[2]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[3]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[4]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[5]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		
+		$contextDataObjects = array();
+		foreach ($specs as $spec)
+		{
+			\spectrum\_internal\getRootSpec()->bindChildSpec($spec);
+			$spec->test->setFunction(function() use(&$contextDataObjects, $spec){
+				$contextDataObjects[] = $spec->test->getContextData();
+			});
+		}
+		
+		\spectrum\_internal\getRootSpec()->run();
+
+		$this->assertSame(array(), get_object_vars($contextDataObjects[0]));
+		$this->assertSame(array(0 => 'aaa'), get_object_vars($contextDataObjects[1]));
+		$this->assertSame(array(0 => 'bbb', 1 => 'ccc'), get_object_vars($contextDataObjects[2]));
+		$this->assertSame(array(0 => 'aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'), get_object_vars($contextDataObjects[3]));
+		$this->assertSame(array(0 => 'bbb1', 'bbb2' => 'bbb3', 'bbb4' => 'bbb5'), get_object_vars($contextDataObjects[4]));
+		$this->assertSame(array(0 => 'ccc1', 'ccc2' => 'ccc3', 'ccc4' => 'ccc5'), get_object_vars($contextDataObjects[5]));
+	}
+	
+	public function providerElementIsArray_KeysAsNames()
+	{
+		return array(
+			array(array('bbb'), array('bbb' => array('aaa'))),
+			array(array('AA bb CC'), array('AA bb CC' => array('aaa', 'bbb'))),
+			array(array('AA bb CC', 'BB bb', 'CC cc'), array(
+				'AA bb CC' => array('aaa', 'bbb'),
+				'BB bb' => array('aaa', 'bbb'),
+				'CC cc' => array('aaa', 'bbb'),
+			)),
+		);
+	}
+	
+	/**
+	 * @dataProvider providerElementIsArray_KeysAsNames
+	 */
+	public function testCallsAtBuildingState_ElementIsArray_ElementKeyIsNotEmptyString_SetsElementKeyAsSpecName($expectedNames, $contexts)
+	{
+		$names = array();
+		foreach (\spectrum\_internal\convertArrayWithContextsToSpecs($contexts) as $spec)
+			$names[] = $spec->getName();
+		
+		$this->assertSame($expectedNames, $names);
+	}
+	
+	public function providerElementIsArray_FirstValueAsNames()
+	{
+		return array(
+			array(array('aaa'), array('' => array('aaa'))),
+			array(array('aaa'), array(0 => array('aaa'))),
+			array(array('aaa'), array(0 => array('a' => 'aaa'))),
+			array(array('aaa'), array(1 => array('aaa'))),
+			array(array('aaa'), array(1 => array('aaa', 'bbb'))),
+			array(array('aaa', 'ccc', 'eee'), array(
+				0 => array('a' => 'aaa', 'b' => 'bbb'),
+				1 => array('c' => 'ccc', 'd' => 'ddd'),
+				2 => array('e' => 'eee'),
+			)),
+			
+			array(array('AA bb CC'), array(0 => array('a' => 'AA bb CC', 'b' => 'bbb'))),
+			
+			array(array(123), array('' => array(123))),
+			array(array(123), array(0 => array(123))),
+			array(array(123), array(1 => array(123))),
+			array(array(123), array(1 => array(123, 'aaa'))),
+			
+			array(array(str_repeat('a', 100)), array(null => array(str_repeat('a', 100)))),
+			array(array(str_repeat('a', 100) . '...'), array(null => array(str_repeat('a', 101)))),
+			array(array(str_repeat('a', 100) . '...'), array(null => array(str_repeat('a', 200)))),
+		);
+	}
+	
+	/**
+	 * @dataProvider providerElementIsArray_FirstValueAsNames
+	 */
+	public function testCallsAtBuildingState_ElementIsArray_ElementKeyIsIntegerOrEmptyString_SetsFirstValueAsSpecName($expectedNames, $contexts)
+	{
+		$names = array();
+		foreach (\spectrum\_internal\convertArrayWithContextsToSpecs($contexts) as $spec)
+			$names[] = $spec->getName();
+		
+		$this->assertSame($expectedNames, $names);
+	}
+	
+/**/
+	
+	public function testCallsAtBuildingState_ElementIsFunction_ReturnsSpecsWithBeforeContextFunctions()
+	{
+		$specs = \spectrum\_internal\convertArrayWithContextsToSpecs(array(
+			function(){},
+			function() use(&$isCalled) { $isCalled = true; },
+			function(){ \spectrum\builders\this()->aaa = 'bbb'; },
 		));
 		
 		$this->assertSame(3, count($specs));
@@ -32,36 +155,120 @@ class ConvertArrayWithContextsToSpecsTest extends \spectrum\tests\Test
 		$contexts = $specs[2]->contextModifiers->getAll();
 		$this->assertSame(1, count($contexts));
 		$this->assertSame('before', $contexts[0]['type']);
-	}
-	
-	public function testCallsAtBuildingState_CreatesContextFunctionsWithElementKeyAsContextDataPropertyName()
-	{
-		$specs = \spectrum\_internal\convertArrayWithContextsToSpecs(array(
-			array('aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'),
-			array('bbb1', 'bbb2' => 'bbb3', 'bbb4' => 'bbb5'),
-			array('ccc1', 'ccc2' => 'ccc3', 'ccc4' => 'ccc5'),
-		));
 		
-		\spectrum\_internal\getRootSpec()->bindChildSpec($specs[0]);
-		\spectrum\_internal\getRootSpec()->bindChildSpec($specs[1]);
-		\spectrum\_internal\getRootSpec()->bindChildSpec($specs[2]);
+		$this->assertSame(null, $isCalled);
 		
 		$contextDataObjects = array();
 		foreach ($specs as $spec)
 		{
+			\spectrum\_internal\getRootSpec()->bindChildSpec($spec);
 			$spec->test->setFunction(function() use(&$contextDataObjects, $spec){
 				$contextDataObjects[] = $spec->test->getContextData();
 			});
 		}
 		
 		\spectrum\_internal\getRootSpec()->run();
-
-		$this->assertSame(array('aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'), get_object_vars($contextDataObjects[0]));
-		$this->assertSame(array('bbb1', 'bbb2' => 'bbb3', 'bbb4' => 'bbb5'), get_object_vars($contextDataObjects[1]));
-		$this->assertSame(array('ccc1', 'ccc2' => 'ccc3', 'ccc4' => 'ccc5'), get_object_vars($contextDataObjects[2]));
+		
+		$this->assertSame(true, $isCalled);
+		$this->assertSame(array(), get_object_vars($contextDataObjects[0]));
+		$this->assertSame(array(), get_object_vars($contextDataObjects[1]));
+		$this->assertSame(array('aaa' => 'bbb'), get_object_vars($contextDataObjects[2]));
 	}
 	
-	public function testCallsAtBuildingState_ElementIsNotArray_ThrowsException()
+	public function providerElementIsFunction_KeysAsNames()
+	{
+		return array(
+			array(array('aaa'), array('aaa' => function(){})),
+			array(array('aaa', 'bbb', 'ccc'), array(
+				'aaa' => function(){},
+				'bbb' => function(){},
+				'ccc' => function(){},
+			)),
+			
+			array(array('AA bb CC'), array('AA bb CC' => function(){})),
+			
+			array(array(''), array('' => function(){})),
+			array(array(0), array(0 => function(){})),
+			array(array(1), array(1 => function(){})),
+		);
+	}
+	
+	/**
+	 * @dataProvider providerElementIsFunction_KeysAsNames
+	 */
+	public function testCallsAtBuildingState_ElementIsFunction_SetsElementKeyAsSpecName($expectedNames, $contexts)
+	{
+		$names = array();
+		foreach (\spectrum\_internal\convertArrayWithContextsToSpecs($contexts) as $spec)
+			$names[] = $spec->getName();
+		
+		$this->assertSame($expectedNames, $names);
+	}
+	
+/**/
+	
+	public function testCallsAtBuildingState_ElementsAreArraysAndFunctions_ReturnsSpecsWithBeforeContextFunctions()
+	{
+		$specs = \spectrum\_internal\convertArrayWithContextsToSpecs(array(
+			array(),
+			function() use(&$isCalled) { $isCalled = true; },
+			array('bbb', 'ccc'),
+			function(){},
+			function(){ \spectrum\builders\this()->aaa = 'bbb'; },
+			array('aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'),
+		));
+		
+		$this->assertSame(6, count($specs));
+		
+		$contexts = $specs[0]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[1]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[2]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[3]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[4]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$contexts = $specs[5]->contextModifiers->getAll();
+		$this->assertSame(1, count($contexts));
+		$this->assertSame('before', $contexts[0]['type']);
+		
+		$this->assertSame(null, $isCalled);
+		
+		$contextDataObjects = array();
+		foreach ($specs as $spec)
+		{
+			\spectrum\_internal\getRootSpec()->bindChildSpec($spec);
+			$spec->test->setFunction(function() use(&$contextDataObjects, $spec){
+				$contextDataObjects[] = $spec->test->getContextData();
+			});
+		}
+		
+		\spectrum\_internal\getRootSpec()->run();
+		
+		$this->assertSame(true, $isCalled);
+		$this->assertSame(array(), get_object_vars($contextDataObjects[0]));
+		$this->assertSame(array(), get_object_vars($contextDataObjects[1]));
+		$this->assertSame(array(0 => 'bbb', 1 => 'ccc'), get_object_vars($contextDataObjects[2]));
+		$this->assertSame(array(), get_object_vars($contextDataObjects[3]));
+		$this->assertSame(array('aaa' => 'bbb'), get_object_vars($contextDataObjects[4]));
+		$this->assertSame(array(0 => 'aaa1', 'aaa2' => 'aaa3', 'aaa4' => 'aaa5'), get_object_vars($contextDataObjects[5]));
+	}
+	
+/**/
+	
+	public function testCallsAtBuildingState_ElementIsNotArrayAndNotFunction_ThrowsException()
 	{
 		$this->assertThrowsException('\spectrum\Exception', 'The context row #2 should be an array', function(){
 			\spectrum\_internal\convertArrayWithContextsToSpecs(array(
@@ -69,35 +276,5 @@ class ConvertArrayWithContextsToSpecsTest extends \spectrum\tests\Test
 				'some name 2' => 'aaa',
 			));
 		});
-	}
-	
-	public function provider()
-	{
-		return array(
-			array('aaa', array('' => array('aaa'))),
-			array('aaa', array(0 => array('aaa'))),
-			array('aaa', array(1 => array('aaa'))),
-			array('aaa', array(1 => array('aaa', 'bbb'))),
-			
-			array(123, array('' => array(123))),
-			array(123, array(0 => array(123))),
-			array(123, array(1 => array(123))),
-			array(123, array(1 => array(123, 'aaa'))),
-			
-			array('bbb', array('bbb' => array('aaa'))),
-			
-			array(str_repeat('a', 100), array(null => array(str_repeat('a', 100)))),
-			array(str_repeat('a', 100) . '...', array(null => array(str_repeat('a', 101)))),
-			array(str_repeat('a', 100) . '...', array(null => array(str_repeat('a', 200)))),
-		);
-	}
-	
-	/**
-	 * @dataProvider provider
-	 */
-	public function testCallsAtBuildingState_SetsToSpecProperName($expectedName, $contexts)
-	{
-		$specs = \spectrum\_internal\convertArrayWithContextsToSpecs($contexts);
-		$this->assertSame($expectedName, $specs[0]->getName());
 	}
 }
