4e1b378846dfe385660e4e66f5d8516ecc704328
Mihail Haritonov <mail@m-haritonov.net>
2014-09-28 23:13:56 +0400

    Improve recursion handling in reports

--- spectrum/core/plugins/reports/drivers/html/components/code/variable.php
+++ spectrum/core/plugins/reports/drivers/html/components/code/variable.php
@@ -31,30 +31,36 @@ class variable extends \spectrum\core\plugins\reports\drivers\html\components\co
 		$previousVariableDepth = array_search($variable, static::$previousVariables, true);
 
 		if ($previousVariableDepth !== false && $previousVariableDepth < $depth) {
-			return static::callComponentMethod('code\variables\recursionVar', 'getContent', array($variable));	
+			$result = static::callComponentMethod('code\variables\recursionVar', 'getContent', array($variable));	
 		} else if ($type === 'boolean') {
-			return static::callComponentMethod('code\variables\boolVar', 'getContent', array($variable));
+			$result = static::callComponentMethod('code\variables\boolVar', 'getContent', array($variable));
 		} else if ($type === 'integer') {
-			return static::callComponentMethod('code\variables\intVar', 'getContent', array($variable));
+			$result = static::callComponentMethod('code\variables\intVar', 'getContent', array($variable));
 		} else if ($type === 'double') {
-			return static::callComponentMethod('code\variables\floatVar', 'getContent', array($variable));
+			$result = static::callComponentMethod('code\variables\floatVar', 'getContent', array($variable));
 		} else if ($type === 'string') {
-			return static::callComponentMethod('code\variables\stringVar', 'getContent', array($variable, $inputCharset));
+			$result = static::callComponentMethod('code\variables\stringVar', 'getContent', array($variable, $inputCharset));
 		} else if ($type === 'array') {
-			return static::callComponentMethod('code\variables\arrayVar', 'getContent', array($variable, $depth, $inputCharset));
+			$result = static::callComponentMethod('code\variables\arrayVar', 'getContent', array($variable, $depth, $inputCharset));
 		} else if ($type === 'object') {
 			$closure = function(){};
 			if ($variable instanceof $closure) {
-				return static::callComponentMethod('code\variables\functionVar', 'getContent', array($variable, $inputCharset));
+				$result = static::callComponentMethod('code\variables\functionVar', 'getContent', array($variable, $inputCharset));
 			} else {
-				return static::callComponentMethod('code\variables\objectVar', 'getContent', array($variable, $depth, $inputCharset));
+				$result = static::callComponentMethod('code\variables\objectVar', 'getContent', array($variable, $depth, $inputCharset));
 			}
 		} else if ($type === 'resource') {
-			return static::callComponentMethod('code\variables\resourceVar', 'getContent', array($variable, $inputCharset));
+			$result = static::callComponentMethod('code\variables\resourceVar', 'getContent', array($variable, $inputCharset));
 		} else if ($type === 'null') {
-			return static::callComponentMethod('code\variables\nullVar', 'getContent');
+			$result = static::callComponentMethod('code\variables\nullVar', 'getContent');
 		} else {
-			return static::callComponentMethod('code\variables\unknownVar', 'getContent', array($variable, $inputCharset));
+			$result = static::callComponentMethod('code\variables\unknownVar', 'getContent', array($variable, $inputCharset));
 		}
+		
+		if ($depth == 0) {
+			static::$previousVariables = array();
+		}
+		
+		return $result;
 	}
 }
